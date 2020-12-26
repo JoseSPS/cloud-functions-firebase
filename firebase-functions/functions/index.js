69188679f9650a565a2d396b14736b589a004e8e
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-12-25 18:38:54 -0600

    upvoting function, notification error

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -39,15 +39,15 @@ exports.userDeleted = functions.auth.user().onDelete(user => {
 });
 
 // http callable function (adding a request)
-exports.addRequest = functions.https.onCall((data, context)=> {
-    if(!context.auth){
+exports.addRequest = functions.https.onCall((data, context) => {
+    if (!context.auth) {
         throw new functions.https.HttpsError(
             'unauthenticated',
             'only authenticated users can add requests'
         );
     }
 
-    if(data.text.length > 30){
+    if (data.text.length > 30) {
         throw new functions.https.HttpsError(
             'invalid-argument',
             'request must be no more than 30 characters long'
@@ -57,5 +57,42 @@ exports.addRequest = functions.https.onCall((data, context)=> {
     return admin.firestore().collection('requests').add({
         text: data.text,
         upvotes: 0,
+    });
+});
+
+
+// // upvote callable function
+exports.upvote = functions.https.onCall((data, context) => {
+    // check auth state
+    if (!context.auth) {
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'only authenticated users can vote up requests'
+        );
+    }
+    // get refs for user doc & request doc
+    const user = admin.firestore().collection('users').doc(context.auth.uid);
+    const request = admin.firestore().collection('requests').doc(data.id);
+
+    return user.get().then(doc => {
+        // check thew user hasn't already upvoted
+        if (doc.data().upvoted.includes(data.id)) {
+            throw new functions.https.HttpsError(
+                'failed-precondition',
+                'You can only vote something up once'
+            );
+        }
+
+        // update the array in user document
+        return user.update({
+            upvoted: [...doc.data().upvoted, data.id]
+        })
+            .then(() => {
+                // update the votes on the request
+                return request.update({
+                    upvotes: admin.firestore.FieldValue.increment(1)
+                });
+            });
+
     });
 });
